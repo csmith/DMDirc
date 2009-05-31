d388566905aca061b4d46c39b5694e32b78fda5c
Gregory Holmes <greg@dmdirc.com>
2009-05-31 14:10:04 +0100

    Window menu can scroll

--- src/com/dmdirc/addons/ui_swing/framemanager/windowmenu/WindowMenuFrameManager.java
+++ src/com/dmdirc/addons/ui_swing/framemanager/windowmenu/WindowMenuFrameManager.java
@@ -31,6 +31,7 @@ import com.dmdirc.ui.interfaces.FrameManager;
 import com.dmdirc.ui.interfaces.Window;
 import com.dmdirc.addons.ui_swing.UIUtilities;
 
+import com.dmdirc.addons.ui_swing.components.MenuScroller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Map;
@@ -116,6 +117,8 @@ public final class WindowMenuFrameManager extends JMenu implements FrameManager,
 
         itemCount = getMenuComponentCount();
         checkToggleState();
+
+        MenuScroller.setScrollerFor(this, 20);
     }
 
     /**

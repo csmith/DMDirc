bf28fea2dc4f24261423784b08fe06c70bc33a21
Shane Mc Cormack <shane@dmdirc.com>
2009-07-14 23:07:08 +0100

    Custom Twitter stuff

---
+++ src/com/dmdirc/addons/twitter/api/TwitterMessage.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dmdirc.addons.twitter.api;
+
+/**
+ *
+ * @author shane
+ */
+public class TwitterMessage {
+    /**
+     *
+     * @return
+     */
+    public String getSenderScreenName() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public TwitterUser getSender() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public long getID() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getText() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ src/com/dmdirc/addons/twitter/api/TwitterStatus.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dmdirc.addons.twitter.api;
+
+/**
+ *
+ * @author shane
+ */
+public class TwitterStatus {
+
+    /**
+     *
+     * @param message
+     */
+    public TwitterStatus(String message) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public long getID() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *
+     * @return
+     */
+    public TwitterUser getUser() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getText() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setReplyTo(long id) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
